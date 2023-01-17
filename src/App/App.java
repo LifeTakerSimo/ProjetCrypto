d73f439bc6018e0fc6144fb0a5402dd337f64bb3
mohamed Kabbou <mohamed.kabbou@uha.fr>
2023-01-17 23:11:36 +0100

    Code avec perf ; all fixed

--- src/App/App.java
+++ src/App/App.java
@@ -6,17 +6,16 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 public class App extends Application {
 
+	private static double totalPerformance = 0;
+
 	public void start(final Stage primaryStage) {
-		Button buttonPerf = new Button();
-		buttonPerf.setTranslateX(0);
-		buttonPerf.setTranslateY(0);
 
 		Button button1 = new Button();
 		button1.setTranslateX(150);
@@ -40,42 +39,60 @@ public class App extends Application {
 		buttonGCM.setTranslateY(-150);
 		buttonGCM.setMaxSize(150,50);
 
-		double totalPerformance = 0;
 
 		buttonGCM.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				EncryptorAesGcmPasswordFile EncryptorAesGcmPasswordFile = new EncryptorAesGcmPasswordFile();
 				try {
-					//totalPerformance += EncryptorAesGcmPasswordFile.cryptDecrypt();
+					double perfGCM = EncryptorAesGcmPasswordFile.cryptDecrypt();
+					App.totalPerformance += perfGCM;
+					System.out.println(totalPerformance);
+
 				} catch (Exception e) {
 					throw new RuntimeException(e);
 				}
 			}
-				});
+		});
 		StackPane root = new StackPane();
 		root.getChildren().add(buttonGCM);
 		root.getChildren().add(button1);
 		root.getChildren().add(button2);
 		root.getChildren().add(button3);
 		root.getChildren().add(button4);
+
+		Button buttonPerf = new Button();
+		buttonPerf.setTranslateX(0);
+		buttonPerf.setTranslateY(0);
+
 		root.getChildren().add(buttonPerf);
-
-
-
 		buttonPerf.setText("Performance");
-		Text perfEncryptor = new Text();
-		perfEncryptor.setText("Performance en Cryptage : " + String.valueOf(totalPerformance) + " kilobytes/ms" + "\n" + "Performance en Decryptage : " +  " kilobytes/ms");
-		perfEncryptor.setTranslateX(150);
-		perfEncryptor.setTranslateY(150);
 
+		Label perfValue = new Label();
+		buttonPerf.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				perfValue.setText("Performance en Cryptage : " + Double.toString(totalPerformance) + " kilobytes/ms" );
+				root.getChildren().add(perfValue);
+				perfValue.setTranslateX(0);
+				perfValue.setTranslateY(400);
+
+			}
+		});
+
+		Button Clean = new Button();
+		Clean.setText("Clean");
+		Clean.setTranslateX(200);
+		Clean.setTranslateY(400);
+		Clean.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				root.getChildren().remove(perfValue);
+			}
+		});
+		root.getChildren().add(Clean);
 		Scene scene = new Scene(root, 800, 900);
 		primaryStage.setTitle("Projet Cryptographie");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-
-		System.out.println(totalPerformance);
 	}
-
 	public static void main(String[] args) {
 		launch(args);
 	}
